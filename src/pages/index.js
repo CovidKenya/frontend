e462c1851a1d1972cb4eb19ec02d389c5eff97b3
paulonteri <onteripaul@gmail.com>
2020-05-14 12:43:01 +0300

    Added example chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ExampleChart from "../components/ExampleChart"
 
 const IndexPage = () => (
   <Layout>
@@ -10,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome</p>
     <p>Now go build something great.</p>
+    <ExampleChart />
     <Link to="/app/">Go to page "app"</Link>
   </Layout>
 )
